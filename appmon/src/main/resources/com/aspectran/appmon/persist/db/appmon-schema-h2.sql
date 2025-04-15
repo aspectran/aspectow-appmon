5add23d9f05f7e9d1e9988c46ec0d2432a370c04
Juho Jeong <aspectran@gmail.com>
2025-04-15 12:40:49 +0900

    Update

--- appmon/src/main/resources/com/aspectran/appmon/persist/db/appmon-schema-h2.sql
+++ appmon/src/main/resources/com/aspectran/appmon/persist/db/appmon-schema-h2.sql
@@ -1,8 +1,11 @@
--- alter table appmon_event_count
---     rename to appmon_event_count_bak;
---
--- alter table appmon_event_count_bak
---     rename constraint appmon_event_count_pk to appmon_event_count_bak_pk;
+alter table APPMON_EVENT_COUNT_LAST
+    alter column UPD_DT rename to REG_DT;
+
+alter table appmon_event_count
+    rename to appmon_event_count_bak;
+
+alter table appmon_event_count_bak
+    rename constraint appmon_event_count_pk to appmon_event_count_bak_pk;
 
 create table if not exists appmon_event_count (
     domain varchar(30) not null,
@@ -15,15 +18,15 @@ create table if not exists appmon_event_count (
     constraint appmon_event_count_pk primary key (domain, instance, event, datetime)
 );
 
--- insert into appmon_event_count select domain, instance, event, concat(ymd, hh, mm), total, delta, 0 from appmon_event_count_bak;
---
--- drop table appmon_event_count_bak;
---
--- alter table appmon_event_count_last
---     rename to appmon_event_count_last_bak;
---
--- alter table appmon_event_count_last_bak
---     rename constraint appmon_event_count_last_pk to appmon_event_count_last_bak_pk;
+insert into appmon_event_count select domain, instance, event, concat(ymd, hh, mm), total, delta, 0 from appmon_event_count_bak;
+
+drop table appmon_event_count_bak;
+
+alter table appmon_event_count_last
+    rename to appmon_event_count_last_bak;
+
+alter table appmon_event_count_last_bak
+    rename constraint appmon_event_count_last_pk to appmon_event_count_last_bak_pk;
 
 create table if not exists appmon_event_count_last (
     domain varchar(30) not null,
@@ -37,6 +40,6 @@ create table if not exists appmon_event_count_last (
     constraint appmon_event_count_last_pk primary key (domain, instance, event)
 );
 
--- insert into appmon_event_count_last select domain, instance, event, concat(ymd, hh, mm), total, delta, 0, reg_dt from appmon_event_count_last_bak;
---
--- drop table appmon_event_count_last_bak;
+insert into appmon_event_count_last select domain, instance, event, concat(ymd, hh, mm), total, delta, 0, reg_dt from appmon_event_count_last_bak;
+
+drop table appmon_event_count_last_bak;
