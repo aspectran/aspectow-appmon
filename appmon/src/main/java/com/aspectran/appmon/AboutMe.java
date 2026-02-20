af3e57c0312a843cc5e9759a83b50847560e75d3
Juho Jeong <aspectran@gmail.com>
2026-02-20 23:14:13 +0900

    Update

--- appmon/src/main/java/com/aspectran/appmon/AboutMe.java
+++ appmon/src/main/java/com/aspectran/appmon/AboutMe.java
@@ -46,7 +46,7 @@ public class AboutMe {
                 pkg.getImplementationVersion() != null) {
             VERSION = pkg.getImplementationVersion();
         } else {
-            VERSION = System.getProperty("appmon.version", "3.0.x");
+            VERSION = System.getProperty("appmon.version", "3.1.x");
         }
 
         POWERED_BY = "Powered by Aspectran " + VERSION;
--- appmon/src/main/resources/com/aspectran/appmon/persist/db/appmon-schema-h2.sql
+++ appmon/src/main/resources/com/aspectran/appmon/persist/db/appmon-schema-h2.sql
@@ -1,3 +1,4 @@
+-- Raw event count data (typically 5-minute intervals)
 create table if not exists appmon_event_count (
     domain varchar(30) not null,
     instance varchar(30) not null,
@@ -9,6 +10,16 @@ create table if not exists appmon_event_count (
     constraint appmon_event_count_pk primary key (domain, instance, event, datetime)
 );
 
+comment on table appmon_event_count is 'Raw event count data';
+comment on column appmon_event_count.domain is 'Monitoring domain name';
+comment on column appmon_event_count.instance is 'Application instance name';
+comment on column appmon_event_count.event is 'Event name';
+comment on column appmon_event_count.datetime is 'Data point timestamp';
+comment on column appmon_event_count.total is 'Cumulative total count (Gauge)';
+comment on column appmon_event_count.delta is 'Incremental count for the interval (Counter)';
+comment on column appmon_event_count.error is 'Incremental error count for the interval';
+
+-- Hourly aggregated event count data
 create table if not exists appmon_event_count_hourly (
     domain varchar(30) not null,
     instance varchar(30) not null,
@@ -20,6 +31,13 @@ create table if not exists appmon_event_count_hourly (
     constraint appmon_event_count_hourly_pk primary key (domain, instance, event, datetime)
 );
 
+comment on table appmon_event_count_hourly is 'Hourly aggregated event count data';
+comment on column appmon_event_count_hourly.datetime is 'Hourly truncated timestamp';
+comment on column appmon_event_count_hourly.total is 'Cumulative total count at the end of the hour';
+comment on column appmon_event_count_hourly.delta is 'Total incremental count for the hour';
+comment on column appmon_event_count_hourly.error is 'Total incremental error count for the hour';
+
+-- Daily aggregated event count data
 create table if not exists appmon_event_count_daily (
     domain varchar(30) not null,
     instance varchar(30) not null,
@@ -31,6 +49,13 @@ create table if not exists appmon_event_count_daily (
     constraint appmon_event_count_daily_pk primary key (domain, instance, event, datetime)
 );
 
+comment on table appmon_event_count_daily is 'Daily aggregated event count data';
+comment on column appmon_event_count_daily.datetime is 'Daily truncated timestamp';
+comment on column appmon_event_count_daily.total is 'Cumulative total count at the end of the day';
+comment on column appmon_event_count_daily.delta is 'Total incremental count for the day';
+comment on column appmon_event_count_daily.error is 'Total incremental error count for the day';
+
+-- Most recent event count state for incremental updates
 create table if not exists appmon_event_count_last (
     domain varchar(30) not null,
     instance varchar(30) not null,
@@ -42,3 +67,6 @@ create table if not exists appmon_event_count_last (
     reg_dt timestamp default now() not null,
     constraint appmon_event_count_last_pk primary key (domain, instance, event)
 );
+
+comment on table appmon_event_count_last is 'Most recent event count state';
+comment on column appmon_event_count_last.reg_dt is 'Database registration timestamp';
