73ed134485fc237dc5e8bdca80840a400a2ff28d
juho.jeong <aspectran@gmail.com>
2026-04-02 15:34:27 +0900

    Add Aspectow AppMon 3.3 assets.

---
+++ assets/appmon@3.3/js/polling-client.js
@@ -0,0 +1,155 @@
+/*
+ * Aspectow AppMon 3.3
+ * Last modified: 2026-03-20
+ */
+
+/**
+ * HTTP Polling implementation of the AppMon client.
+ */
+class PollingClient extends BaseClient {
+    constructor(domain, viewer, onJoined, onEstablished, onClosed, onFailed) {
+        super(domain, viewer, onJoined, onEstablished, onClosed, onFailed);
+        this.endpointMode = "polling";
+        this.commands = [];
+        this.pollingTimer = null;
+        this.stopped = false;
+    }
+
+    start(instancesToJoin) {
+        this.stopped = false;
+        this.join(instancesToJoin);
+    }
+
+    stop() {
+        this.stopped = true;
+        if (this.pollingTimer) {
+            clearTimeout(this.pollingTimer);
+            this.pollingTimer = null;
+        }
+    }
+
+    speed(speed) {
+        this.changePollingInterval(speed);
+    }
+
+    refresh(options) {
+        let cmdOptions = ["command:refresh"];
+        if (options) {
+            cmdOptions.push(...options);
+        }
+        this.sendCommand(cmdOptions);
+    }
+
+    sendCommand(options) {
+        if (options) {
+            options.forEach(option => this.withCommand(option));
+        }
+    }
+
+    withCommand(command) {
+        if (!this.commands.includes(command)) {
+            this.commands.push(command);
+        }
+    }
+
+    join(instancesToJoin) {
+        $.ajax({
+            url: this.domain.endpoint.url + "/polling/join",
+            type: "post",
+            dataType: "json",
+            data: {
+                timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+                instances: instancesToJoin
+            },
+            success: (data) => {
+                if (data) {
+                    this.retryCount = 0;
+                    this.domain.endpoint['mode'] = this.endpointMode;
+                    this.domain.endpoint['pollingInterval'] = data.pollingInterval;
+                    if (this.onJoined) {
+                        this.onJoined(this.domain, data);
+                    }
+                    if (this.onEstablished) {
+                        this.onEstablished(this.domain);
+                    }
+                    this.viewer.printMessage("Polling every " + data.pollingInterval + " milliseconds.");
+                    this.polling(instancesToJoin);
+                } else {
+                    console.log(this.domain.name, "connection failed");
+                    this.viewer.printErrorMessage("Connection failed.");
+                    this.rejoin(instancesToJoin);
+                }
+            },
+            error: (xhr, status, error) => {
+                console.log(this.domain.name, "connection failed", error);
+                this.viewer.printErrorMessage("Connection failed.");
+                this.rejoin(instancesToJoin);
+            }
+        });
+    }
+
+    polling(instancesToJoin) {
+        if (this.stopped) return;
+        let withCommands = null;
+        if (this.commands.length) {
+            withCommands = this.commands.slice();
+            this.commands.length = 0;
+        }
+        $.ajax({
+            url: this.domain.endpoint.url + "/polling/pull",
+            type: "get",
+            cache: false,
+            data: withCommands ? {
+                commands: withCommands
+            } : null,
+            success: (data) => {
+                if (this.stopped) return;
+                if (data && data.messages) {
+                    data.messages.forEach(msg => this.viewer.processMessage(msg));
+                    this.pollingTimer = setTimeout(() => {
+                        this.polling(instancesToJoin);
+                    }, this.domain.endpoint.pollingInterval);
+                } else {
+                    console.log(this.domain.name, "connection lost");
+                    this.viewer.printErrorMessage("Connection lost.");
+                    if (this.onClosed) {
+                        this.onClosed(this.domain);
+                    }
+                    this.rejoin(instancesToJoin);
+                }
+            },
+            error: (xhr, status, error) => {
+                if (this.stopped) return;
+                console.log(this.domain.name, "connection lost", error);
+                this.viewer.printErrorMessage("Connection lost.");
+                if (this.onClosed) {
+                    this.onClosed(this.domain);
+                }
+                this.rejoin(instancesToJoin);
+            }
+        });
+    }
+
+    changePollingInterval(speed) {
+        $.ajax({
+            url: this.domain.endpoint.url + "/polling/interval",
+            type: "post",
+            dataType: "json",
+            data: { speed: speed },
+            success: (data) => {
+                if (data && data.pollingInterval) {
+                    this.domain.endpoint.pollingInterval = data.pollingInterval;
+                    console.log(this.domain.name, "pollingInterval", data.pollingInterval);
+                    this.viewer.printMessage("Polling every " + data.pollingInterval + " milliseconds.");
+                } else {
+                    console.log(this.domain.name, "failed to change polling interval");
+                    this.viewer.printMessage("Failed to change polling interval.");
+                }
+            },
+            error: (xhr, status, error) => {
+                console.log(this.domain.name, "failed to change polling interval", error);
+                this.viewer.printMessage("Failed to change polling interval.");
+            }
+        });
+    }
+}
