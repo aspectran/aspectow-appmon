cb2c8a669bb0f16fa8a3be6806d64cb18e331f0a
juho.jeong <aspectran@gmail.com>
2026-04-02 15:56:09 +0900

    Polishing

--- appmon/src/main/java/com/aspectran/aspectow/appmon/common/auth/AppMonCookieIssuer.java
+++ appmon/src/main/java/com/aspectran/aspectow/appmon/common/auth/AppMonCookieIssuer.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2008-2024 The Aspectran Project
+ * Copyright (c) 2020-present The Aspectran Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
