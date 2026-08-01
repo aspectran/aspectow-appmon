608b047bf3de0d56f872ff00cb523363e39fb3b1
Juho Jeong <aspectran@gmail.com>
2026-08-01 16:19:04 +0900

    Add ConsoleWebUtils for extracting remote client IP address
    Introduced `ConsoleWebUtils` utility class with a `getRemoteAddr` method to retrieve the remote client IP, supporting `X-Forwarded-For` headers for proxies/load balancers. This enhances request handling capabilities in Aspectow Console.

---
+++ console/src/main/java/com/aspectran/aspectow/console/common/util/ConsoleWebUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020-present The Aspectran Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aspectran.aspectow.console.common.util;
+
+import com.aspectran.core.activity.Translet;
+import com.aspectran.utils.StringUtils;
+import com.aspectran.web.support.http.HttpHeaders;
+import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * Utility class providing web-related helper methods for Aspectow Console.
+ */
+public abstract class ConsoleWebUtils {
+
+    /**
+     * Extracts the remote client IP address from the request.
+     * Checks the X-Forwarded-For header first for proxies/load balancers,
+     * falling back to the remote address of the underlying servlet request.
+     * @param translet the current translet
+     * @return the remote IP address
+     */
+    public static String getRemoteAddr(@NonNull Translet translet) {
+        String remoteAddr = translet.getRequestAdapter().getHeader(HttpHeaders.X_FORWARDED_FOR);
+        if (StringUtils.hasLength(remoteAddr)) {
+            if (remoteAddr.contains(",")) {
+                remoteAddr = StringUtils.tokenize(remoteAddr, ",", true)[0];
+            }
+        } else {
+            remoteAddr = ((HttpServletRequest)translet.getRequestAdaptee()).getRemoteAddr();
+        }
+        return remoteAddr;
+    }
+
+}
