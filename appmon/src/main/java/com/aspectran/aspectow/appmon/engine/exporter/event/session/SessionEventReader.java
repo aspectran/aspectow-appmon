142857bfd72132964c37c3e76926c05fd2f585cd
Juho Jeong <aspectran@gmail.com>
2026-09-08 00:59:01 +0900

    fix(appmon): use remaining inactive interval for session event data
    - Replace `getMaxInactiveInterval` with `getRemainingInactiveInterval` to provide accurate session timeout details in `SessionEventReader`.

--- appmon/src/main/java/com/aspectran/aspectow/appmon/engine/exporter/event/session/SessionEventReader.java
+++ appmon/src/main/java/com/aspectran/aspectow/appmon/engine/exporter/event/session/SessionEventReader.java
@@ -437,7 +437,7 @@ public class SessionEventReader extends AbstractEventReader {
                     .put("countryCode", session.getAttribute(USER_COUNTRY_CODE))
                     .put("activityCount", (count != null ? count.get() : 0))
                     .put("createAt", formatTime(session.getCreationTime()))
-                    .put("inactiveInterval", session.getMaxInactiveInterval())
+                    .put("inactiveInterval", session.getRemainingInactiveInterval())
                     .put("tempResident", session.isTempResident())
                 .endObject()
                 .toJsonString();
